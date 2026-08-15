87ba50d86ffc591eef79c09fce2a347f1a6ab75e
iki1uc <entertrainer@web.de>
2026-08-15 13:26:51 +0200

    Add files via upload

---
+++ CORE/CORE_Operator.js
@@ -0,0 +1,3 @@
+export function CORE_OP(x){
+    return Math.log10(Math.abs(x) + 1);
+}
---
+++ CORE/CORE_mass.js
@@ -0,0 +1,4 @@
+export const CORE_MASS = {
+    real_blackhole: 4.3e6,   // Sagittarius A*
+    golem_blackhole: 1e5     // frei definierbar
+};
